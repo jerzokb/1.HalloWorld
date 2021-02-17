2e8bc0d67b7cb2319af5fd89993c058f92b4305f
jerzokb <beata@palkabm.net>
2021-02-17 21:31:00 +0100

    Initial commit

---
+++ witaj.ts
@@ -0,0 +1,10 @@
+console.log("Hello Jan");
+console.log("How do you do?");
+console.log("       *       ");
+console.log("      ***      ");
+console.log("     *****     ");
+console.log("    *******    ");
+console.log("   *********   ");
+console.log("  ***********  ");
+console.log(" ************* ");
+console.log("***************");
